781ef15a735f73604fa4e19e5546c8eea4b4ae6b
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 11:57:02 +0200

    Update chat component to default chat schema

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
-import { ChatService } from '../chat.service';
+import { AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @Component({
-  selector: 'app-chat',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+    selector: 'app-chat',
+    standalone: true,
+    imports: [CommonModule, FormsModule, MatIconModule, MatTooltipModule],
+    templateUrl: './chat.component.html',
+    styleUrl: './chat.component.css'
 })
-export class ChatComponent {
-    
+export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
+
+    @ViewChild('container') container!: ElementRef;
+
+    messages: Message[] = []
+    newMessage = '';
+    waitingForResponse = false;
+    connected = false;
+
     constructor(private chatService: ChatService) { }
+
+    ngOnInit(): void {
+        this.chatService.connect();
+        this.chatService.connected().subscribe(connected => this.connected = connected);
+    }
+
+    ngOnDestroy(): void {
+        this.chatService.disconect();
+    }
+
+    sendMessage() {
+        this.messages.push({ author: "user", "content": this.newMessage });
+        if (this.newMessage.trim().length > 0) {
+            this.chatService.send(this.newMessage);
+            this.waitingForResponse = true;
+            this.newMessage = '';
+        }
+    }
+
+    ngAfterViewChecked(): void {
+        this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    }
 }
