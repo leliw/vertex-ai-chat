d4f9a168b1acd46852a370ab17d5fed75b17a986
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 19:31:42 +0200

    Chat messages are show as markdown

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,17 +1,19 @@
-import { AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ChatService, Message } from '../chat.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MarkdownPipe } from '../shared/markdown.pipe';
 
 
 @Component({
     selector: 'app-chat',
     standalone: true,
-    imports: [CommonModule, FormsModule, MatIconModule, MatTooltipModule],
+    imports: [CommonModule, FormsModule, MatIconModule, MatTooltipModule, MarkdownPipe],
     templateUrl: './chat.component.html',
-    styleUrl: './chat.component.css'
+    styleUrl: './chat.component.css',
+    encapsulation: ViewEncapsulation.None,
 })
 export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
 
