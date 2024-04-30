a3fbb893bbc7eed7d6b67af3c4e0fb5d2c96fd43
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 11:46:22 +0200

    Add ChatService to ChatComponent constructor

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
-
+    
+    constructor(private chatService: ChatService) { }
 }
