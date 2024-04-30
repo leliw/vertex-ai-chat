93e100aacc95d2524207d43c23a0a09149cf20ac
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 11:47:47 +0200

    Update chat route to redirect to '/chat' by default

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/chat', pathMatch: 'full' },
     { path: 'chat', component: ChatComponent },
 ];
