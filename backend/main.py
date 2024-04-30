f481e66de67014e99ed4e0435a618c15fd68a9e5
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 11:40:54 +0200

    Remove backend sample endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
 """Main file for FastAPI server"""
 
 import datetime
-import json
-from typing import Annotated, List, Optional, Union
-from fastapi import Depends, FastAPI, HTTPException, Request, Response
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from pyaml_env import parse_config
 
 from gcp_oauth import OAuth
 from gcp_secrets import GcpSecrets
 from gcp_session import SessionManager, SessionData as BaseSessionData
-from movies import Movie
 from static_files import static_file_response
 
 
@@ -39,28 +37,6 @@ async def auth_google(code: str, response: Response):
     return user_data
 
 
-# @app.middleware("http")
-# async def verify_token_middleware(request: Request, call_next):
-#     return await oAuth.verify_token_middleware(request, call_next)
-
-
-# async def session_reader(request: Request, response: Response) -> SessionData:
-#     try:
-#         data = await session_manager(request)
-#         return data
-#     except InvalidSessionException:
-#         pass
-#     except HTTPException as e:
-#         if e.status_code != 403 or e.detail != "No session provided":
-#             raise e
-#     user_data = oAuth.verify_token(request)
-#     if not user_data:
-#         raise HTTPException(status_code=401, detail="Unauthorized")
-#     data = SessionData(user=user_data)
-#     await session_manager.create_session(response, data)
-#     return data
-
-
 SessionDataDep = Annotated[SessionData, Depends(session_manager.session_reader)]
 
 
@@ -73,58 +49,6 @@ async def user_get(session_data: SessionDataDep):
 async def read_config():
     """Return config from yaml file"""
     return config
-
-
-@app.get("/api")
-async def read_root():
-    """Return Hello World"""
-    return {"Hello": "World"}
-
-
-@app.get("/api/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    """Return item_id and q"""
-    return {"item_id": item_id, "q": q}
-
-
-with open("movies.json", "r", encoding="utf-8") as file:
-    movies_data = json.load(file)
-movies = {f"{movie['title']}_{movie['year']}": Movie(**movie) for movie in movies_data}
-
-
-@app.get("/api/movies", response_model=List[Movie])
-async def get_all_movies(session_data: SessionDataDep):
-    print(session_data)
-    return [movie for movie in movies.values()]
-
-
-@app.post("/api/movies", response_model=Movie)
-async def add_movie(movie: Movie):
-    key = f"{movie.title}_{movie.year}"
-    movies[key] = movie.model_dump()
-    location = f"/api/movies/{key}"
-    return Response(status_code=201, headers={"Location": location})
-
-
-@app.get("/api/movies/{key}", response_model=Movie)
-async def get_movie(key: str):
-    if key not in movies:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    return movies[key]
-
-
-@app.put("/api/movies/{key}")
-async def update_movie(key: str, movie: Movie):
-    if key not in movies:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    movies[key] = movie.model_dump()
-
-
-@app.delete("/api/movies/{key}")
-async def delete_movie(key: str):
-    if key not in movies:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    movies.pop(key)
 
 
 # Angular static files - it have to be at the end of file
