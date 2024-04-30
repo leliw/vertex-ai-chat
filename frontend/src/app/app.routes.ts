e3722f185f01a6a790aa62620360af56a5aefbfd
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-04-30 11:47:16 +0200

    Add chat route to app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
+    { path: 'chat', component: ChatComponent },
 ];
