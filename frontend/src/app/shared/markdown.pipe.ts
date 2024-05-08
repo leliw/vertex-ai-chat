0c32c52eb6c887687506da51a44920f24108d0a7
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-05-08 20:28:47 +0200

    Markdown code highlight

--- frontend/src/app/shared/markdown.pipe.ts
+++ frontend/src/app/shared/markdown.pipe.ts
@@ -1,6 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { marked } from 'marked';
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
 
+const marked = new Marked(
+    markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang, info) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        }
+    })
+);
 
 @Pipe({
     name: 'markdown',
@@ -9,7 +20,7 @@ import { marked } from 'marked';
 export class MarkdownPipe implements PipeTransform {
     transform(value: any, args?: any[]): any {
         if (value && value.length > 0) {
-            return marked(value);
+            return marked.parse(value);
         }
         return value;
     }
