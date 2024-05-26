ede23851a55ec107d0ef45d58db5be584bb91d8b
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-05-26 11:55:04 +0200

    KnowledgeBaseTable - generated

---
+++ frontend/src/app/knowledge-base/knowledge-base-table/knowledge-base-table.component.ts
@@ -0,0 +1,28 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { KnowledgeBaseTableDataSource, KnowledgeBaseTableItem } from './knowledge-base-table-datasource';
+
+@Component({
+  selector: 'app-knowledge-base-table',
+  templateUrl: './knowledge-base-table.component.html',
+  styleUrl: './knowledge-base-table.component.css',
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+})
+export class KnowledgeBaseTableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<KnowledgeBaseTableItem>;
+  dataSource = new KnowledgeBaseTableDataSource();
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
