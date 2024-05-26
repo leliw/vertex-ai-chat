8b36a56755a9e70225be12c1728810dc5df6f163
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-05-26 13:46:53 +0200

    KnowledgeBaseForm - creating new item

--- frontend/src/app/knowledge-base/knowledge-base-form/knowledge-base-form.component.ts
+++ frontend/src/app/knowledge-base/knowledge-base-form/knowledge-base-form.component.ts
@@ -6,103 +6,80 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
+import { KnowledgeBaseItem, KnowledgeBaseService } from '../knowledge-base.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
-  selector: 'app-knowledge-base-form',
-  templateUrl: './knowledge-base-form.component.html',
-  styleUrl: './knowledge-base-form.component.css',
-  standalone: true,
-  imports: [
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    ReactiveFormsModule
-  ]
+    selector: 'app-knowledge-base-form',
+    templateUrl: './knowledge-base-form.component.html',
+    styleUrl: './knowledge-base-form.component.css',
+    standalone: true,
+    imports: [
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatCardModule,
+        ReactiveFormsModule
+    ]
 })
 export class KnowledgeBaseFormComponent {
-  private fb = inject(FormBuilder);
-  addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    ],
-    shipping: ['free', Validators.required]
-  });
+    private fb = inject(FormBuilder);
+    form = this.fb.group({
+        title: ['', Validators.required],
+        content: ['', Validators.required],
+        keywords: [''],
+    });
+    itemId: number = 0;
+    editMode: boolean = false;
 
-  hasUnitNumber = false;
+    constructor(private knowledgeBaseService: KnowledgeBaseService, private route: ActivatedRoute,
+        private router: Router,) { }
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
+    onSubmit(): void {
+        if (this.form.invalid) {
+            return;
+        }
+        const formData = this.form.value as unknown as KnowledgeBaseItem;
+        // formData.id = this.itemId;
+        // Split keywords string into an array
+        formData.keywords = this.form.value.keywords ? this.form.value.keywords.split(',').map((keyword: string) => keyword.trim()) : [];
+        if (this.editMode) {
+            this.updateItem(formData);
+        } else {
+            this.createItem(formData);
+        }
+    }
 
-  onSubmit(): void {
-    alert('Thanks!');
-  }
+    onCancel(): void {
+        alert('Cancelled');
+    }
+
+    createItem(itemData: KnowledgeBaseItem) {
+        this.knowledgeBaseService.createItem(itemData).subscribe({
+            next: () => {
+                // Handle success, e.g., navigate to the item list
+                this.router.navigate(['/knowledge-base']);
+            },
+            error: (error) => {
+                console.error('Error creating item:', error);
+                // Handle error
+            }
+        });
+    }
+
+    updateItem(itemData: KnowledgeBaseItem) {
+        this.knowledgeBaseService.updateItem(this.itemId, itemData).subscribe({
+            next: () => {
+                // Handle success, e.g., navigate to the item list
+                this.router.navigate(['/knowledge-base']);
+            },
+            error: (error) => {
+                console.error('Error updating item:', error);
+                // Handle error
+            }
+        });
+    }
+
 }
