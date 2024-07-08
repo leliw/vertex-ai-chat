55176aa949c443916a0a62968376095b1a9b5d35
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-07-08 22:43:55 +0200

    chore: Add CRUD operations for agents in agent_service and agents router

--- backend/app/agent/agent_service.py
+++ backend/app/agent/agent_service.py
@@ -13,3 +13,15 @@ class AgentService:
     def get_all(self, user_email: str):
         storage = self._crete_storage(user_email)
         return storage.keys()
+
+    def get(self, user_email: str, agent_name: str):
+        storage = self._crete_storage(user_email)
+        return storage.get(agent_name)
+
+    def put(self, user_email: str, agent_name: str, agent: Agent):
+        storage = self._crete_storage(user_email)
+        storage.put(agent_name, agent)
+
+    def delete(self, user_email: str, agent_name: str):
+        storage = self._crete_storage(user_email)
+        storage.delete(agent_name)
--- backend/app/routers/agents.py
+++ backend/app/routers/agents.py
@@ -7,6 +7,7 @@ service = AgentService()
 router = APIRouter(
     tags=["agent"],
 )
+AGENT_PATH = "/{agemt_name}"
 
 
 @router.post("", response_model=Agent)
@@ -22,3 +23,26 @@ def get_all(request: Request):
     """Returns a list of AI agents for current user."""
     user_email = request.state.session_data.user.email
     return service.get_all(user_email)
+
+
+@router.get(AGENT_PATH, response_model=Agent)
+def get(request: Request, agent_name: str):
+    """Returns an AI agent by name."""
+    user_email = request.state.session_data.user.email
+    agent = service.get(user_email, agent_name)
+    return agent
+
+
+@router.put(AGENT_PATH, response_model=Agent)
+def put(request: Request, agent_name: str, agent: Agent):
+    """Updates an AI agent."""
+    user_email = request.state.session_data.user.email
+    service.put(user_email, agent_name, agent)
+    return agent
+
+
+@router.delete(AGENT_PATH)
+def delete(request: Request, agent_name: str):
+    """Deletes an AI agent."""
+    user_email = request.state.session_data.user.email
+    service.delete(user_email, agent_name)
