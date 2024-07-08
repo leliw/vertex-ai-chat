b029f90acd1e022c4cdda5424dfaf91fbf8e3230
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-07-08 22:29:38 +0200

    feat: Add get_all agents

--- backend/app/agent/agent_service.py
+++ backend/app/agent/agent_service.py
@@ -3,7 +3,13 @@ from .agent_model import Agent
 
 
 class AgentService:
+    def _crete_storage(self, user_email: str):
+        return Storage(f"user/{user_email}/agent", Agent, key_name="name")
 
     def create(self, user_email: str, agent: Agent):
-        storage = Storage(f"user/{user_email}/agent", Agent, key_name="name")
+        storage = self._crete_storage(user_email)
         storage.save(agent)
+
+    def get_all(self, user_email: str):
+        storage = self._crete_storage(user_email)
+        return storage.keys()
--- backend/app/routers/agents.py
+++ backend/app/routers/agents.py
@@ -15,3 +15,10 @@ def create(request: Request, agent: Agent):
     user_email = request.state.session_data.user.email
     service.create(user_email, agent)
     return agent
+
+
+@router.get("", response_model=list[str])
+def get_all(request: Request):
+    """Returns a list of AI agents for current user."""
+    user_email = request.state.session_data.user.email
+    return service.get_all(user_email)
