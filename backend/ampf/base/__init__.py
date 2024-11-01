b377378393ace337735ba82b9edc88b251b01897
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-11-01 20:45:38 +0100

    feat(ampf.base) Add BaseEmailSender

--- backend/ampf/base/__init__.py
+++ backend/ampf/base/__init__.py
@@ -1,4 +1,5 @@
 from .base_storage import BaseStorage, T, KeyExists, KeyNotExists
+from .base_email_sender import BaseEmailSender
 from .base_blob_storage import BaseBlobStorage
 from .ampf_base_factory import AmpfBaseFactory
 from . import logger
@@ -7,6 +8,7 @@ from .singleton import singleton
 __all__ = [
     "BaseStorage",
     "BaseBlobStorage",
+    "BaseEmailSender",
     "T",
     "KeyExists",
     "KeyNotExists",
