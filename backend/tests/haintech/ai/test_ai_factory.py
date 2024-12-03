899c79e3edb5f5a9c4ee21f202ceca810a734f44
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-12-03 22:21:09 +0100

    refactor(test) AiFactory

---
+++ backend/tests/haintech/ai/test_ai_factory.py
@@ -0,0 +1,39 @@
+import pytest
+from vertexai.generative_models import GenerativeModel, ChatSession, Content, Part
+from haintech.ai import AiFactory
+
+
+@pytest.fixture
+def ai_factory():
+    return AiFactory()
+
+
+model_name = "gemini-1.5-flash"
+
+
+def test_get_model(ai_factory):
+    model = ai_factory.get_model(model_name)
+    assert isinstance(model, GenerativeModel)
+
+
+def test_get_chat(ai_factory):
+    chat = ai_factory.get_chat(model_name)
+    assert isinstance(chat, ChatSession)
+
+
+def test_get_chat_with_history(ai_factory):
+    history = [
+        Content(role="user", parts=[Part.from_text("Hello")]),
+        Content(role="model", parts=[Part.from_text("Hi")]),
+        Content(role="user", parts=[Part.from_text("How are you?")]),
+    ]
+    chat = ai_factory.get_chat(model_name, history)
+    assert isinstance(chat, ChatSession)
+
+
+def test_send_message(ai_factory):
+    chat = ai_factory.get_chat(model_name)
+    resp = chat.send_message("Who was the first president of the United States?")
+    assert isinstance(chat.history, list)
+    assert isinstance(chat.history[0], Content)
+    assert isinstance(resp.text, str)
