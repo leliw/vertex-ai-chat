43701cabba4d1142c95cc3a21c14c4e2869b4787
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-12-19 16:57:16 +0100

    chore(version) 0.6.6

--- backend/app/config.py
+++ backend/app/config.py
@@ -45,7 +45,7 @@ Jeśli nie prosiłeś o zresetowanie hasła, zignoruj ten email.
 class ServerConfig(BaseSettings):
     model_config = SettingsConfigDict(env_nested_delimiter="__")
 
-    version: str = "0.6.5"
+    version: str = "0.6.6"
 
     jwt_secret_key: str
     default_user: DefaultUserConfig = DefaultUserConfig()
