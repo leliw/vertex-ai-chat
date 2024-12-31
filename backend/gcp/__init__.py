fc5fccee2e2d044e35df1944359c0d30001dff5c
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-01-01 00:43:48 +0100

    refactor(gcp) Remove secrets and session

--- backend/gcp/__init__.py
+++ backend/gcp/__init__.py
@@ -1,10 +1,6 @@
 from .gcp_oauth import OAuth
-from .gcp_secrets import GcpSecrets
-from .gcp_file_storage import FileStorage
 
 
 __all__ = [
     "OAuth",
-    "GcpSecrets",
-    "FileStorage",
 ]
